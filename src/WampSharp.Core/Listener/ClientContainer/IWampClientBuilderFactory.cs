e4da015c67ec9c5eba2c170d14d5c0638070954d
darkl <dark_leizar@walla.com>
2013-04-25 00:08:26 +0300

    Removed some files

--- src/WampSharp.Core/Listener/ClientContainer/IWampClientBuilderFactory.cs
+++
@@ -1,7 +0,0 @@
-namespace WampSharp.Core.Listener
-{
-    public interface IWampClientBuilderFactory<TConnection>
-    {
-        IWampClientBuilder<TConnection> GetClientBuilder(IWampClientContainer<TConnection> container);
-    }
-}
