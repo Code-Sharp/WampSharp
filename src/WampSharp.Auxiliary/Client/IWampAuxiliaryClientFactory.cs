8b4beceaeb416c8198b5ff478d692be15a56b453
darkl <dark_leizar@walla.com>
2013-07-06 19:29:39 +0300

    Moved classes to separate files

--- src/WampSharp.Auxiliary/Client/IWampAuxiliaryClientFactory.cs
+++ src/WampSharp.Auxiliary/Client/IWampAuxiliaryClientFactory.cs
@@ -1,26 +1,9 @@
-﻿using WampSharp.Core.Client;
-using WampSharp.Core.Contracts.V1;
-using WampSharp.Core.Listener;
+﻿using WampSharp.Core.Listener;
 
 namespace WampSharp.Auxiliary
 {
     public interface IWampAuxiliaryClientFactory<TMessage>
     {
         IWampClientConnectionMonitor CreateMonitor(IWampConnection<TMessage> connection);
-    }
-
-    public class WampAuxiliaryClientFactory<TMessage> : IWampAuxiliaryClientFactory<TMessage>
-    {
-        private readonly IWampServerProxyBuilder<TMessage, IWampAuxiliaryClient, IWampServer> mServerProxyBuilder;
-
-        public WampAuxiliaryClientFactory(IWampServerProxyBuilder<TMessage, IWampAuxiliaryClient, IWampServer> serverProxyBuilder)
-        {
-            mServerProxyBuilder = serverProxyBuilder;
-        }
-
-        public IWampClientConnectionMonitor CreateMonitor(IWampConnection<TMessage> connection)
-        {
-            return new WampClientConnectionMonitor<TMessage>(mServerProxyBuilder, connection);
-        }
     }
 }
