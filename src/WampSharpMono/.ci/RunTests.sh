a5a62ac155587e082b8b1c450d7358575e980e42
darkl <dark_leizar@walla.com>
2013-07-09 01:52:08 +0300

    Adding unit tests to build

---
+++ src/WampSharpMono/.ci/RunTests.sh
@@ -0,0 +1,15 @@
+#!/bin/sh -x
+
+mono --runtime=v4.0 .nuget/NuGet.exe install NUnit.Runners -Version 2.6.1 -o packages
+
+runTest(){
+    mono --runtime=v4.0 packages/NUnit.Runners.2.6.1/tools/nunit-console.exe -noxml -nodots -labels -stoponerror $@
+   if [ $? -ne 0 ]
+   then   
+     exit 1
+   fi
+}
+
+runTest WampSharp.Tests/bin/Debug/WampSharp.Tests.dll -exclude=Performance
+
+exit $?
