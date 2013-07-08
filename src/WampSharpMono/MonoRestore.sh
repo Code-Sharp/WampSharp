54863065bb48f0638aefce3a9e6a125e8ad9f1a3
darkl <dark_leizar@walla.com>
2013-07-08 13:55:31 +0300

    xbuild now really passes

---
+++ src/WampSharpMono/MonoRestore.sh
@@ -0,0 +1,7 @@
+#!/bin/sh -x
+mkdir -p packages
+cd packages
+for i in WampSharp.Api WampSharp.Auxiliary WampSharp.Core WampSharp.Fleck WampSharp.PubSub WampSharp.Rpc WampSharp.Tests WampSharp.WebSocket4Net
+  do mono --runtime=v4.0 ../.nuget/NuGet.exe install ../$i/packages.config
+done
+cd ..
