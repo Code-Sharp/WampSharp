7b81f5d6aff16a92e37c057fa17cc699c3a4870e
Elad Zelingher <dark_leizar@walla.com>
2013-07-09 11:43:56 +0300

    Unit test run won't stop on error

--- src/WampSharpMono/.ci/RunTests.sh
+++ src/WampSharpMono/.ci/RunTests.sh
@@ -3,7 +3,7 @@
 mono --runtime=v4.0 .nuget/NuGet.exe install NUnit.Runners -Version 2.6.1 -o packages
 
 runTest(){
-    mono --runtime=v4.0 packages/NUnit.Runners.2.6.1/tools/nunit-console.exe -noxml -nodots -labels -stoponerror $@
+    mono --runtime=v4.0 packages/NUnit.Runners.2.6.1/tools/nunit-console.exe -noxml -nodots -labels $@
    if [ $? -ne 0 ]
    then   
      exit 1
