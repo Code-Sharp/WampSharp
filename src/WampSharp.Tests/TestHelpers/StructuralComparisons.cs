1cee5017a726a57930a1806b8b0cdbb739778e7d
darkl <dark_leizar@walla.com>
2013-07-09 11:39:50 +0300

    StructuralComparisons patch for mono
    It isn't implemented correctly, so it fails my unit tests :(

---
+++ src/WampSharp.Tests/TestHelpers/StructuralComparisons.cs
@@ -0,0 +1,20 @@
+﻿#if MONO
+
+using System.Collections;
+
+namespace WampSharp.Tests.TestHelpers
+{
+
+    public static class StructuralComparisons
+    {
+        private static readonly IEqualityComparer mComparer = new StructuralEqualityComparer();
+
+        public static IEqualityComparer StructuralEqualityComparer
+        {
+            get { return mComparer; }
+        }
+    }
+
+}
+
+#endif
---
+++ src/WampSharp.Tests/TestHelpers/StructuralEqualityComparer.cs
@@ -0,0 +1,39 @@
+﻿#if MONO
+
+using System.Collections;
+using System.Collections.Generic;
+
+namespace WampSharp.Tests.TestHelpers
+{
+    internal class StructuralEqualityComparer : IEqualityComparer<IStructuralEquatable>,
+                                                IEqualityComparer
+    {
+        public bool Equals(IStructuralEquatable x, IStructuralEquatable y)
+        {
+            if (x == null)
+            {
+                return (y == null);
+            }
+
+            return x.Equals(y, EqualityComparer<object>.Default);
+        }
+
+        public int GetHashCode(IStructuralEquatable obj)
+        {
+            return obj.GetHashCode(EqualityComparer<object>.Default);
+        }
+
+        public bool Equals(object x, object y)
+        {
+            return this.Equals(x as IStructuralEquatable,
+                               y as IStructuralEquatable);
+        }
+
+        public int GetHashCode(object obj)
+        {
+            return GetHashCode(obj as IStructuralEquatable);
+        }
+    }
+}
+
+#endif
