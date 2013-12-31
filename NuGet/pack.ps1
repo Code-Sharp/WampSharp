3313bd9ef54879a462c612bac1e98e234194af2a
Elad Zelingher <dark_leizar@walla.com>
2013-12-31 20:38:23 +0200

    Update pack.ps1

--- NuGet/pack.ps1
+++ NuGet/pack.ps1
@@ -9,22 +9,22 @@ Param (
 
 # update package version in nuspec file
 Write-Output "Updating version in WampSharp nuspec file"
-$nuspecPath = "$srcFolder\WampSharp.nuspec"
+$nuspecPath = "WampSharp.nuspec"
 [xml]$xml = Get-Content $nuspecPath
 $xml.package.metadata.version = $projectVersion
 $xml.Save($nuspecPath)
 
 # update package version in nuspec file
 Write-Output "Updating version in WampSharp.Default nuspec file"
-$nuspecPath = "$srcFolder\WampSharp.Default.nuspec"
+$nuspecPath = "WampSharp.Default.nuspec"
 [xml]$xml = Get-Content $nuspecPath
 $xml.package.metadata.version = $projectVersion
 $xml.Save($nuspecPath)
 
 # build NuGet package
 Write-Output "Building WampSharp NuGet package"
-..\src\.nuget\NuGet.exe pack WampSharp.nuspec
+..\src\.nuget\NuGet.exe pack .\WampSharp.nuspec
 
 # build NuGet package
 Write-Output "Building WampSharp.Default NuGet package"
-..\src\.nuget\NuGet.exe pack WampSharp.Default.nuspec
+..\src\.nuget\NuGet.exe pack .\WampSharp.Default.nuspec
