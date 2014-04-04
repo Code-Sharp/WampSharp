ae52714b576faff0dafc43c83c6cae4133383750
darkl <dark_leizar@walla.com>
2014-04-05 01:07:05 +0300

    Console sample playground

--- src/Samples/WampSharp.RpcServerSample/Program.cs
+++ src/Samples/WampSharp.RpcServerSample/Program.cs
@@ -1,12 +1,19 @@
 ﻿using System;
-using WampSharp.V1;
-using WampSharp.V1.Rpc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using MsgPack;
+using Newtonsoft.Json.Linq;
+using WampSharp.Binding;
+using WampSharp.V2;
+using WampSharp.V2.Client;
+using WampSharp.V2.Rpc;
+using WampSharp.WebSocket4Net;
 
 namespace WampSharp.RpcServerSample
 {
     public interface ICalculator
     {
-        [WampRpcMethod("http://example.com/simple/calc#add")]
+        [WampProcedure("com.arguments.add2")]
         int Add(int x, int y);
     }
 
@@ -24,17 +31,85 @@ namespace WampSharp.RpcServerSample
         {
             // http://autobahn.ws/static/file/autobahnjs.html
 
-            const string location = "ws://localhost:9000/";
-            using (IWampHost host = new DefaultWampHost(location))
+            const string location = "ws://localhost:8080/ws";
+            using (DefaultWampHost host = new DefaultWampHost(location))
             {
-                ICalculator instance = new Calculator();
-                host.HostService(instance);
-
                 host.Open();
 
+                Calculator instance = new Calculator();
+
+                var binding = new JTokenBinding();
+
+                WampChannelFactory<JToken> channelFactory = 
+                    new WampChannelFactory<JToken>(binding);
+
+                WampChannel<JToken> channel =
+                    channelFactory.CreateChannel
+                        ("realm1",
+                         new WebSocket4NetTextConnection<JToken>("ws://localhost:8080/ws",
+                                                                 binding));
+
+                channel.Open().Wait();
+
+                Task register =
+                    channel.RealmProxy.RpcCatalog.Register
+                        (new SyncMethodInfoRpcOperation(instance,
+                                                        typeof (ICalculator).GetMethod("Add")),
+                         new {});
+
+                register.Wait();
+
+
+                var messagePackObjectBinding = new MessagePackObjectBinding();
+
+                WampChannelFactory<MessagePackObject> channelFactory2 =
+                    new WampChannelFactory<MessagePackObject>
+                        (messagePackObjectBinding);
+
+                var channel2 =
+                    channelFactory2.CreateChannel
+                        ("realm1",
+                         new WebSocket4NetBinaryConnection<MessagePackObject>("ws://localhost:8080/ws",
+                                                                 messagePackObjectBinding));
+
+                channel2.Open().Wait();
+
+                channel2.RealmProxy.RpcCatalog.Invoke
+                    (new MyCallback(),
+                     new Dictionary<string, string>(),
+                     "com.arguments.add2",
+                     new object[]{1024,768});
+                       
                 Console.WriteLine("Server is running on " + location);
                 Console.ReadLine();
             }
+        }
+    }
+
+    internal class MyCallback : IWampRpcOperationCallback
+    {
+        public void Result(object details)
+        {
+        }
+
+        public void Result(object details, object[] arguments)
+        {
+        }
+
+        public void Result(object details, object[] arguments, object argumentsKeywords)
+        {
+        }
+
+        public void Error(object details, string error)
+        {
+        }
+
+        public void Error(object details, string error, object[] arguments)
+        {
+        }
+
+        public void Error(object details, string error, object[] arguments, object argumentsKeywords)
+        {
         }
     }
 }
