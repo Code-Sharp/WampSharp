f43873bb47ae4a2273080647ee9ca49d5f3b9bf6
darkl <dark_leizar@walla.com>
2014-04-17 13:09:30 +0300

    Removed redundant method

--- src/WampSharp/WAMP2/V2/Binding/IWampBinaryBinding.cs
+++ src/WampSharp/WAMP2/V2/Binding/IWampBinaryBinding.cs
@@ -1,11 +1,9 @@
-﻿using WampSharp.Core.Message;
-using WampSharp.V2.Binding.Parsers;
+﻿using WampSharp.V2.Binding.Parsers;
 
 namespace WampSharp.V2.Binding
 {
     public interface IWampBinaryBinding<TMessage> : IWampBinding<TMessage>,
         IWampBinaryMessageParser<TMessage>
     {
-        WampMessage<TMessage> Parse(byte[] bytes);
     }
 }
