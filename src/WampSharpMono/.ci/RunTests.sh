884e910cd9c3d5ad1c4080cff0bcba8721aa68d0
darkl <dark_leizar@walla.com>
2014-05-02 21:33:46 +0300

    Folder rearrangement

--- src/WampSharpMono/.ci/RunTests.sh
+++ src/WampSharpMono/.ci/RunTests.sh
@@ -1,16 +1,16 @@
 #!/bin/sh -x
 
-mono --runtime=v4.0 .nuget/NuGet.exe install NUnit.Runners -Version 2.6.1 -o packages
+mono --runtime=v4.0 .nuget/NuGet.exe install NUnit.Runners -Version 2.6.3 -o packages
 
 runTest(){
-    mono --runtime=v4.0 packages/NUnit.Runners.2.6.1/tools/nunit-console.exe -noxml -nodots -labels $@
+    mono --runtime=v4.0 packages/NUnit.Runners.2.6.3/tools/nunit-console.exe -noxml -nodots -labels $@
    if [ $? -ne 0 ]
    then   
      exit 1
    fi
 }
 
-runTest WampSharp.Tests/bin/Debug/WampSharp.Tests.dll -exclude=Performance
-runTest WampSharp.Tests.Wampv2/bin/Debug/WampSharp.Tests.Wampv2.dll -exclude=Performance
+runTest Tests/WampSharp.Tests/bin/Debug/WampSharp.Tests.dll -exclude=Performance
+runTest Tests/WampSharp.Tests.Wampv2/bin/Debug/WampSharp.Tests.Wampv2.dll -exclude=Performance
 
 exit $?
