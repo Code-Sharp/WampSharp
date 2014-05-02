2e5f2fd56b8d0ceb4a3bad052eae24ceb6dac0e8
darkl <dark_leizar@walla.com>
2014-05-03 00:17:22 +0300

    Try add permissions

--- src/mono/.ci/RunTests.sh
+++ src/mono/.ci/RunTests.sh
@@ -1,9 +1,9 @@
 #!/bin/sh -x
 
-mono --runtime=v4.0 .nuget/NuGet.exe install NUnit.Runners -Version 2.6.1 -o packages
+mono --runtime=v4.0 .nuget/NuGet.exe install NUnit.Runners -Version 2.6.3 -o packages
 
 runTest(){
-    mono --runtime=v4.0 packages/NUnit.Runners.2.6.1/tools/nunit-console.exe -noxml -nodots -labels $@
+    mono --runtime=v4.0 packages/NUnit.Runners.2.6.3/tools/nunit-console.exe -noxml -nodots -labels $@
    if [ $? -ne 0 ]
    then   
      exit 1
