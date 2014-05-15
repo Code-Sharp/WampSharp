8e7c9061d7fa36e43479fad26bf79c7d4a7b91f7
darkl <dark_leizar@walla.com>
2014-05-16 00:55:30 +0300

    Wrong attribute
    The attribute has no meaning, but maybe it will in one of the days.

--- src/net45/WampSharp/WAMP2/V2/Core/Contracts/Error/IWampCalleeError.cs
+++ src/net45/WampSharp/WAMP2/V2/Core/Contracts/Error/IWampCalleeError.cs
@@ -1,26 +1,25 @@
-﻿using WampSharp.Core.Contracts;
-using WampSharp.Core.Message;
+﻿using WampSharp.Core.Message;
 
 namespace WampSharp.V2.Core.Contracts
 {
     public interface IWampCalleeError<TMessage>
     {
-        [WampHandler(WampMessageType.v2Register)]
+        [WampErrorHandler(WampMessageType.v2Register)]
         void RegisterError(long requestId, TMessage details, string error);
 
-        [WampHandler(WampMessageType.v2Register)]
+        [WampErrorHandler(WampMessageType.v2Register)]
         void RegisterError(long requestId, TMessage details, string error, TMessage[] arguments);
 
-        [WampHandler(WampMessageType.v2Register)]
+        [WampErrorHandler(WampMessageType.v2Register)]
         void RegisterError(long requestId, TMessage details, string error, TMessage[] arguments, TMessage argumentsKeywords);
 
-        [WampHandler(WampMessageType.v2Unregister)]
+        [WampErrorHandler(WampMessageType.v2Unregister)]
         void UnregisterError(long requestId, TMessage details, string error);
 
-        [WampHandler(WampMessageType.v2Unregister)]
+        [WampErrorHandler(WampMessageType.v2Unregister)]
         void UnregisterError(long requestId, TMessage details, string error, TMessage[] arguments);
 
-        [WampHandler(WampMessageType.v2Unregister)]
+        [WampErrorHandler(WampMessageType.v2Unregister)]
         void UnregisterError(long requestId, TMessage details, string error, TMessage[] arguments, TMessage argumentsKeywords);
     }
 }
--- src/net45/WampSharp/WAMP2/V2/Core/Contracts/Error/IWampCallerError.cs
+++ src/net45/WampSharp/WAMP2/V2/Core/Contracts/Error/IWampCallerError.cs
@@ -1,17 +1,16 @@
-﻿using WampSharp.Core.Contracts;
-using WampSharp.Core.Message;
+﻿using WampSharp.Core.Message;
 
 namespace WampSharp.V2.Core.Contracts
 {
     public interface IWampCallerError<TMessage>
     {
-        [WampHandler(WampMessageType.v2Call)]
+        [WampErrorHandler(WampMessageType.v2Call)]
         void CallError(long requestId, TMessage details, string error);
 
-        [WampHandler(WampMessageType.v2Call)]
+        [WampErrorHandler(WampMessageType.v2Call)]
         void CallError(long requestId, TMessage details, string error, TMessage[] arguments);
 
-        [WampHandler(WampMessageType.v2Call)]
+        [WampErrorHandler(WampMessageType.v2Call)]
         void CallError(long requestId, TMessage details, string error, TMessage[] arguments, TMessage argumentsKeywords);
     }
 }
