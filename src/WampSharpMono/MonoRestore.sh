c990d73af29e3999fa3c83bcfb3750d1d0b746b0
darkl <dark_leizar@walla.com>
2014-05-17 16:54:47 +0300

    Fixed Mono package restore

--- src/WampSharpMono/MonoRestore.sh
+++ src/WampSharpMono/MonoRestore.sh
@@ -1,3 +1,9 @@
+# Linux certificate handling
+sudo mozroots --import --machine --sync
+sudo certmgr -ssl -m https://go.microsoft.com
+sudo certmgr -ssl -m https://nugetgallery.blob.core.windows.net
+sudo certmgr -ssl -m https://nuget.org
+
 #!/bin/sh -x
 mkdir -p packages
 cd packages
