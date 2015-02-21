37664f5e0cb99a84794ed9ba1b495956fc8bc9ad
Elad Zelingher <dark_leizar@walla.com>
2015-02-21 13:38:19 +0200

    Creating delegates for publications, so cool!

---
+++ src/net45/PubSubByAttribute/Program.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reactive.Concurrency;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Castle.DynamicProxy;
+using WampSharp.Core.Listener;
+using WampSharp.V2;
+using WampSharp.V2.Binding;
+using WampSharp.V2.Binding.Transports;
+using WampSharp.V2.Client;
+using WampSharp.V2.Core;
+using WampSharp.V2.Core.Contracts;
+using WampSharp.V2.DelegatePubSub;
+using WampSharp.V2.Transports;
+
+namespace PubSubByAttribute
+{
+    public class EventHandlerGenerator
+    {
+        private static readonly MethodInfo mPublishArrayMethod =
+            Method.Get((IWampTopicProxy topicProxy) =>
+                topicProxy.Publish(default(PublishOptions), default(object[])));
+
+        private static readonly MethodInfo mPublishArgumentsMethod =
+            Method.Get((IWampTopicProxy topicProxy) =>
+                topicProxy.Publish(default(PublishOptions), default(object[]), default(IDictionary<string, object>)));
+
+        private static readonly MethodInfo mAddMethod =
+            Method.Get((IDictionary<string, object> dictionary) =>
+                dictionary.Add(default(string), default(object)));
+
+        public TDelegate CreateArrayDelegate<TDelegate>(IWampTopicProxy proxy)
+        {
+            Func<ParameterExpression[], Expression> callExpression = parameters =>
+                Expression.Call(Expression.Constant(proxy), mPublishArrayMethod,
+                    Expression.Constant(new PublishOptions() {}),
+                    Expression.NewArrayInit(typeof (object),
+                        parameters.Select(x => Expression.Convert(x, typeof (object)))));
+
+            return InnerCreateDelegate<TDelegate>(callExpression);
+        }
+
+        public TDelegate CreateArgumentsDelegate<TDelegate>(IWampTopicProxy proxy)
+        {
+            Func<ParameterExpression[], Expression> callExpression = parameters =>
+                Expression.Call(Expression.Constant(proxy), mPublishArgumentsMethod,
+                    Expression.Constant(new PublishOptions() {}),
+                    Expression.NewArrayInit(typeof (object), Enumerable.Empty<Expression>()),
+                    Expression.ListInit(Expression.New(typeof (Dictionary<string, object>)),
+                        parameters.Select(x => Expression.ElementInit(mAddMethod,
+                            Expression.Constant(x.Name, typeof (string)),
+                            Expression.Convert(x, typeof (object))))
+                        ));
+
+            return InnerCreateDelegate<TDelegate>(callExpression);
+        }
+
+        private static TDelegate InnerCreateDelegate<TDelegate>(Func<ParameterExpression[], Expression> callPublishFactory)
+        {
+            MethodInfo method =
+                typeof (TDelegate).GetMethod("Invoke");
+
+            ParameterExpression[] parameters =
+                method.GetParameters()
+                    .Select(x => Expression.Parameter(x.ParameterType, x.Name))
+                    .ToArray();
+
+            Expression callPublish = callPublishFactory(parameters);
+
+            Expression<TDelegate> lambda =
+                Expression.Lambda<TDelegate>(callPublish, parameters);
+
+            TDelegate result = lambda.Compile();
+
+            return result;
+        }
+
+        private bool IsAction(Type type)
+        {
+            return type.Name == "Action";
+        }
+
+        private static class Method
+        {
+            public static MethodInfo Get<T>(Expression<Action<T>> methodCall)
+            {
+                MethodCallExpression callExpression = methodCall.Body as MethodCallExpression;
+
+                return callExpression.Method;
+            }
+        }
+    }
+
+    class Program
+    {
+        public static void MyMethod(IDictionary<string, object> arguments)
+        {
+            
+        }
+
+        static void Main(string[] args)
+        {
+
+            var init =
+                new Expression[]
+                {
+                    Expression.ListInit(Expression.New(typeof (Dictionary<string, object>)), new ElementInit[]
+                    {
+                        Expression.ElementInit(typeof (Dictionary<string, object>).GetMethod("Add"),
+                            new Expression[]
+                            {
+                                Expression.Constant("Key", typeof (string)),
+                                Expression.Convert(Expression.Constant(3, typeof (int)), typeof (object))
+                            })
+                    })
+                };
+
+
+            Expression<Action> a =
+                () => MyMethod(new Dictionary<string, object>() {{"Key", 3}});
+
+            WampHost host = new WampHost();
+            InMemoryTransport inMemoryTransport = new InMemoryTransport(Scheduler.Immediate);
+            IWampBinding<object> inMemoryBinding = new InMemoryBinding();
+            host.RegisterTransport(inMemoryTransport, new IWampBinding[] {inMemoryBinding});
+            host.Open();
+
+            IWampChannel channel = CreateClient(inMemoryTransport, inMemoryBinding);
+            IWampChannel channel2 = CreateClient(inMemoryTransport, inMemoryBinding);
+
+
+            channel.Open();
+            channel2.Open();
+
+            IWampSubject subject = 
+                channel.RealmProxy.Services.GetSubject("com.topic.arguments");
+
+            subject.Subscribe(x =>
+            {
+
+            });
+
+            IWampTopicProxy topicProxy = 
+                channel2.RealmProxy.TopicContainer.GetTopicByUri("com.topic.arguments");
+
+            EventHandlerGenerator generator = new EventHandlerGenerator();
+
+            Action<string, int, int> action =
+                PublisherDelegateFactory<Action<string, int, int>>.CreateArgumentsHandler(topicProxy,
+                    new PublishOptions());
+
+            Action<string, int, int> action2 =
+                PublisherDelegateFactory<Action<string, int, int>>.CreateArrayHandler(topicProxy,
+                    new PublishOptions());
+
+
+            action("Hello", 5, 4);
+            action2("Hello", 5, 4);
+        }
+
+        private static IWampChannel CreateClient(InMemoryTransport inMemoryTransport, IWampBinding<object> inMemoryBinding)
+        {
+            IControlledWampConnection<object> controlledWampConnection =
+                inMemoryTransport.CreateClientConnection(inMemoryBinding, Scheduler.Immediate);
+
+            WampChannelFactory factory = new WampChannelFactory();
+            IWampChannel channel = factory.CreateChannel("realm1", controlledWampConnection, inMemoryBinding);
+            return channel;
+        }
+    }
+}
---
+++ src/net45/WampSharp/WAMP2/V2/Api/DelegatePubSub/EventHandlerGenerator.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using WampSharp.V2.Client;
+using WampSharp.V2.Core.Contracts;
+
+namespace WampSharp.V2.DelegatePubSub
+{
+    public class PublisherDelegateFactory<TDelegate>
+    {
+        private static readonly DelegateFactory<TDelegate> mPublishArrayFactory =
+            EventHandlerGenerator.CreateArrayDelegate<TDelegate>();
+
+        private static readonly DelegateFactory<TDelegate> mPublishArgumentsFactory =
+            EventHandlerGenerator.CreateArgumentsDelegate<TDelegate>();
+
+        public static TDelegate CreateArrayHandler(IWampTopicProxy topicProxy, PublishOptions options)
+        {
+            return mPublishArrayFactory.Invoke(topicProxy, options);
+        }
+
+        public static TDelegate CreateArgumentsHandler(IWampTopicProxy topicProxy, PublishOptions options)
+        {
+            return mPublishArgumentsFactory.Invoke(topicProxy, options);
+        }
+    }
+
+    internal delegate TDelegate DelegateFactory<TDelegate>(IWampTopicProxy topicProxy, PublishOptions options);
+
+
+    internal class EventHandlerGenerator
+    {
+        private static readonly MethodInfo mPublishArrayMethod =
+            Method.Get((IWampTopicProxy topicProxy) =>
+                topicProxy.Publish(default(PublishOptions), default(object[])));
+
+        private static readonly MethodInfo mPublishArgumentsMethod =
+            Method.Get((IWampTopicProxy topicProxy) =>
+                topicProxy.Publish(default(PublishOptions), default(object[]), default(IDictionary<string, object>)));
+
+        private static readonly MethodInfo mAddMethod =
+            Method.Get((IDictionary<string, object> dictionary) =>
+                dictionary.Add(default(string), default(object)));
+
+        public static DelegateFactory<TDelegate> CreateArrayDelegate<TDelegate>()
+        {
+            // Writes: topicProxy.Publish(publishOptions,
+            //                            new object[]
+            //                                {
+            //                                      (object)arg1,
+            //                                      (object)arg2,
+            //                                      ...,
+            //                                      (object)argn
+            //                                });            
+            Func<Expression, Expression, ParameterExpression[], Expression> callExpression = 
+                (topicProxy, publishOptions, parameters) =>
+                Expression.Call(topicProxy, mPublishArrayMethod,
+                    publishOptions,
+                    Expression.NewArrayInit(typeof (object),
+                        parameters.Select(x => Expression.Convert(x, typeof (object)))));
+
+            return InnerCreateDelegate<TDelegate>(callExpression);
+        }
+
+        public static DelegateFactory<TDelegate> CreateArgumentsDelegate<TDelegate>()
+        {
+            // Writes: topicProxy.Publish(publishOptions,
+            //                            new object[0],
+            //                            new Dictionary<string, object>()
+            //                                {
+            //                                  {"@arg1", arg1}, 
+            //                                  {"@arg2", arg2}, 
+            //                                  {"@arg3", arg3},
+            //                                  ....,
+            //                                  {"@argn", argn}
+            //                                });
+            // where @arg1, @arg2, ..., @argn are the names of the delegate variables.
+
+            Func<Expression, Expression, ParameterExpression[], Expression> callExpression =
+                (topicProxy, publishOptions, parameters) =>
+                    Expression.Call(topicProxy, mPublishArgumentsMethod,
+                        publishOptions,
+                        Expression.NewArrayInit(typeof (object), Enumerable.Empty<Expression>()),
+                        Expression.ListInit(Expression.New(typeof (Dictionary<string, object>)),
+                            parameters.Select(x => Expression.ElementInit(mAddMethod,
+                                Expression.Constant(x.Name, typeof (string)),
+                                Expression.Convert(x, typeof (object))))
+                            ));
+
+            return InnerCreateDelegate<TDelegate>(callExpression);
+        }
+
+        private static DelegateFactory<TDelegate> InnerCreateDelegate<TDelegate>(
+            Func<Expression, Expression, ParameterExpression[], Expression> callPublishFactory)
+        {
+            MethodInfo method =
+                typeof (TDelegate).GetMethod("Invoke");
+
+            ValidateMethod(method);
+
+            ParameterExpression topicProxy =
+                Expression.Parameter(typeof (IWampTopicProxy), "topicProxy");
+
+            ParameterExpression publishOptions =
+                Expression.Parameter(typeof(PublishOptions), "publishOptions");
+
+            ParameterExpression[] parameters =
+                method.GetParameters()
+                    .Select(x => Expression.Parameter(x.ParameterType, x.Name))
+                    .ToArray();
+
+            Expression callPublish = callPublishFactory(topicProxy, publishOptions, parameters);
+
+            Expression<TDelegate> lambda =
+                Expression.Lambda<TDelegate>(callPublish, parameters);
+
+            Expression<DelegateFactory<TDelegate>> delegateCreator =
+                Expression.Lambda<DelegateFactory<TDelegate>>(lambda, topicProxy, publishOptions);
+
+            DelegateFactory<TDelegate> result = delegateCreator.Compile();
+
+            return result;
+        }
+
+        private static void ValidateMethod(MethodInfo method)
+        {
+            Type delegateType = method.DeclaringType;
+
+            if (method.ReturnType != typeof(void))
+            {
+                throw new ArgumentException("Expected return type of void from delegate type " + delegateType.FullName);
+            }
+
+            if (method.GetParameters().Any(x => x.IsOut || x.ParameterType.IsByRef))
+            {
+                throw new ArgumentException("Expected no out/ref parameters from delegate type " + delegateType.FullName);                
+            }
+        }
+
+        private static class Method
+        {
+            public static MethodInfo Get<T>(Expression<Action<T>> methodCall)
+            {
+                MethodCallExpression callExpression = methodCall.Body as MethodCallExpression;
+
+                return callExpression.Method;
+            }
+        }
+    }
+}
