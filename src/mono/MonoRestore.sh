2a7d6ad78709d2f9e98226d8aded8c22f440efb7
Elad Zelingher <dark_leizar@walla.com>
2015-02-27 02:14:46 +0200

    Trying to fix mono build

--- src/mono/MonoRestore.sh
+++ src/mono/MonoRestore.sh
@@ -7,7 +7,7 @@ sudo certmgr -ssl -m https://nuget.org
 #!/bin/sh -x
 mkdir -p packages
 cd packages
-for i in .nuget WampSharp WampSharp.Default Tests/WampSharp.Tests Tests/WampSharp.Tests.Wampv2 Samples/WAMP1/WampSharp.PubSubServerSample Samples/WAMP1/WampSharp.RpcClientSample Samples/WAMP1/WampSharp.RpcServerSample Samples/WAMP2/WampSharp.Samples.Callee Extensions/WampSharp.RawTcp Extensions/WampSharp.SignalR
+for i in .nuget WampSharp WampSharp.Default Tests/WampSharp.Tests Tests/WampSharp.Tests.Wampv2 Samples/WAMP1/WampSharp.PubSubServerSample Samples/WAMP1/WampSharp.RpcClientSample Samples/WAMP1/WampSharp.RpcServerSample Samples/WAMP2/WampSharp.Samples.Callee Samples/WAMP2/WampSharp.Samples.EdgeJs Samples/WAMP2/WampSharp.Samples.Authentication Extensions/WampSharp.RawSocket Extensions/WampSharp.SignalR Extensions/WampSharp.Vtortola
   do mono --runtime=v4.0 ../.nuget/NuGet.exe install ../$i/packages.config
 done
 cd ..
