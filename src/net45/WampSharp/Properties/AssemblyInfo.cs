d54ae9fb3f30cf8c85bb521885aca639ad3129b9
Elad Zelingher <dark_leizar@walla.com>
2015-05-11 23:25:37 +0300

    Adding project json

--- src/net45/WampSharp/Properties/AssemblyInfo.cs
+++ src/net45/WampSharp/Properties/AssemblyInfo.cs
@@ -22,7 +22,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("6bb11d9a-8de4-46f5-936d-1a50a9eedbb1")]
+//[assembly: Guid("6bb11d9a-8de4-46f5-936d-1a50a9eedbb1")]
 
 // Version information for an assembly consists of the following four values:
 //
