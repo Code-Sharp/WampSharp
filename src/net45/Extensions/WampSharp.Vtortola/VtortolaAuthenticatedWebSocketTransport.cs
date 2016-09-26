4aa0313ac2d9000e0ef03257f0368b245ad0e2ab
Elad Zelingher <dark_leizar@walla.com>
2016-09-27 00:01:51 +0300

    Added an option to specify WebSocketListenerOptions

--- src/net45/Extensions/WampSharp.Vtortola/VtortolaAuthenticatedWebSocketTransport.cs
+++ src/net45/Extensions/WampSharp.Vtortola/VtortolaAuthenticatedWebSocketTransport.cs
@@ -1,5 +1,6 @@
 using System.Net;
 using System.Security.Cryptography.X509Certificates;
+using vtortola.WebSockets;
 using WampSharp.V2.Authentication;
 
 namespace WampSharp.Vtortola
@@ -7,11 +8,14 @@ namespace WampSharp.Vtortola
     public class VtortolaAuthenticatedWebSocketTransport : VtortolaWebSocketTransport
     {
         public VtortolaAuthenticatedWebSocketTransport
-            (IPEndPoint endpoint,
-             bool perMessageDeflate,
-             ICookieAuthenticatorFactory authenticatorFactory,
-             X509Certificate2 certificate = null)
-            : base(endpoint, perMessageDeflate, authenticatorFactory, certificate)
+            (IPEndPoint endpoint, bool perMessageDeflate, ICookieAuthenticatorFactory authenticatorFactory, WebSocketListenerOptions options)
+            : this(endpoint, perMessageDeflate, authenticatorFactory, null, options)
+        {
+        }
+
+        public VtortolaAuthenticatedWebSocketTransport
+            (IPEndPoint endpoint, bool perMessageDeflate, ICookieAuthenticatorFactory authenticatorFactory, X509Certificate2 certificate = null, WebSocketListenerOptions options = null)
+            : base(endpoint, perMessageDeflate, authenticatorFactory, certificate, options)
         {
         }
     }
