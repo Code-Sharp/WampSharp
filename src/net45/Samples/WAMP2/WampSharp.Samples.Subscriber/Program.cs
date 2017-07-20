2296b26798445781dc062eaec599080f476329d5
Elad Zelingher <dark_leizar@walla.com>
2017-07-21 01:09:22 +0300

    Reverting Subscriber example Program.cs

--- src/net45/Samples/WAMP2/WampSharp.Samples.Subscriber/Program.cs
+++ src/net45/Samples/WAMP2/WampSharp.Samples.Subscriber/Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Reactive.Subjects;
 using WampSharp.V2;
-using WampSharp.V2.Core.Contracts;
 using WampSharp.V2.PubSub;
 using WampSharp.V2.Realm;
 
@@ -12,13 +11,23 @@ namespace WampSharp.Samples.Subscriber
         static void Main(string[] args)
         {
             const string serverAddress = "ws://127.0.0.1:8080/ws";
+            
+            WampHost host = new DefaultWampHost(serverAddress);
 
-            ClientCode(serverAddress);
+            //IDisposable disposable = ServerCode(host);
+
+            host.Open();
+
+            IDisposable disposable = ClientCode(serverAddress);
+
+            Console.ReadLine();
+
+            disposable.Dispose();
 
             Console.ReadLine();
         }
 
-        private static void ClientCode(string serverAddress)
+        private static IDisposable ClientCode(string serverAddress)
         {
             DefaultWampChannelFactory channelFactory = 
                 new DefaultWampChannelFactory();
@@ -28,10 +37,12 @@ namespace WampSharp.Samples.Subscriber
 
             wampChannel.Open().Wait();
 
-                wampChannel.RealmProxy.Services.RegisterSubscriber(new MyHandler(), new SubscriberRegistrationInterceptor(new SubscribeOptions()
-                {
-                    GetRetained = true
-                }));
+            ISubject<int> subject =
+                wampChannel.RealmProxy.Services.GetSubject<int>("com.myapp.topic1");
+
+            IDisposable disposable = subject.Subscribe(x => GetValue(x));
+
+            return disposable;
         }
 
         private static IDisposable ServerCode(WampHost host)
@@ -52,15 +63,6 @@ namespace WampSharp.Samples.Subscriber
         private static void GetValue(int number)
         {
             Console.WriteLine("Got " + number);
-        }
-    }
-
-    internal class MyHandler
-    {
-        [WampTopic("com.myapp.topic1")]
-        public void OnCounter(int value)
-        {
-            Console.WriteLine(value);
         }
     }
 }
