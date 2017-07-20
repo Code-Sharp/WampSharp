989c14dcc0e36b4afabde3852f6143b5a3aa1531
Elad Zelingher <dark_leizar@walla.com>
2017-07-21 01:07:37 +0300

    Reverting Caller example Program.cs changes

--- src/net45/Samples/WAMP2/WampSharp.Samples.Caller/Program.cs
+++ src/net45/Samples/WAMP2/WampSharp.Samples.Caller/Program.cs
@@ -1,55 +1,150 @@
 ﻿using System;
-using System.Reactive.Linq;
-using System.Reactive.Subjects;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using WampSharp.Binding;
+using WampSharp.Samples.Caller.Contracts;
 using WampSharp.V2;
-using WampSharp.V2.Core.Contracts;
-using WampSharp.V2.MetaApi;
+using WampSharp.V2.Realm;
 
-namespace WampSharp.Samples.Publisher
+namespace WampSharp.Samples.Caller
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            const string serverAddress = "ws://127.0.0.1:8080/ws";
+            if (args == null || args.Length == 0)
+            {
+                Console.WriteLine("Usage: WampSharp.Samples.Caller.exe sample [client|router]");
+                Console.WriteLine("Where sample is one of the following values:");
 
-            ClientCode(serverAddress);
+                foreach (string name in
+                    typeof (Program).GetMethods(BindingFlags.NonPublic |
+                                                BindingFlags.Static)
+                                    .Where(x => x.IsDefined(typeof (SampleAttribute), true))
+                                    .Select(x => x.GetCustomAttribute<SampleAttribute>().Name))
+                {
+                    Console.WriteLine(" {0}", name);
+                }
+
+                args = new string[] {"complex", "client"};
+            }
+
+            string sampleName = args[0];
+            string routerOrClient = args[1];
+
+            bool router = StringComparer.InvariantCultureIgnoreCase.Equals(routerOrClient, "router");
+
+            MethodInfo sampleMethod = GetSampleMethod(sampleName);
+
+            string serverAddress = "ws://127.0.0.1:8080/ws";
+
+            if (router)
+            {
+                RouterCode(serverAddress, sampleMethod);
+            }
+            else
+            {
+                ClientCode(serverAddress, sampleMethod);
+            }
+        }
+
+        private static void RouterCode(string serverAddress, MethodInfo sampleMethod)
+        {
+            DefaultWampHost host = new DefaultWampHost(serverAddress);
+
+            IWampHostedRealm realm = host.RealmContainer.GetRealmByName("realm1");
+
+            host.Open();
+
+            Console.WriteLine("Server is up");
+
+            Console.WriteLine("Press any key to start demo.");
+
+            Console.ReadLine();
+
+            sampleMethod.Invoke(null, new object[] { realm.Services });
 
             Console.ReadLine();
         }
 
-        private static IDisposable ClientCode(string serverAddress)
+        private static void ClientCode(string serverAddress, MethodInfo sampleMethod)
         {
-            DefaultWampChannelFactory channelFactory =
+            JTokenJsonBinding binding = new JTokenJsonBinding();
+
+            DefaultWampChannelFactory factory =
                 new DefaultWampChannelFactory();
 
-            IWampChannel wampChannel =
-                channelFactory.CreateJsonChannel(serverAddress, "realm1");
+            IWampChannel channel =
+                factory.CreateChannel(serverAddress, "realm1", binding);
 
-            wampChannel.Open().Wait();
+            Task task = channel.Open();
+            task.Wait(5000);
 
-            IWampSubject subject =
-                wampChannel.RealmProxy.Services.GetSubject("com.myapp.topic1");
+            if (!task.IsCompleted)
+            {
+                Console.WriteLine("Server might be down.");
+                Console.WriteLine("Press any key to exit...");
+                Console.ReadLine();
+                Environment.Exit(0);
+            }
+            else
+            {
+                Console.WriteLine("Connected to server.");
+            }
 
-            int counter = 0;
+            sampleMethod.Invoke(null, new object[] { channel.RealmProxy.Services });
 
-            IObservable<long> timer =
-                Observable.Timer(TimeSpan.FromMilliseconds(0),
-                                 TimeSpan.FromMilliseconds(1000));
-
-            IDisposable disposable =
-                timer.Subscribe(x =>
-                {
-                    counter++;
-                    Console.WriteLine("Publishing to topic 'wamp.myapp.counter': " + counter);
-                    subject.OnNext(new WampEvent()
-                    {
-                        Arguments = new object[]{counter },
-                        Options = new PublishOptions() { Retain = true}
-                    });
-                });
-
-            return disposable;
+            Console.ReadLine();
         }
+
+        private static MethodInfo GetSampleMethod(string sampleName)
+        {
+            return typeof (Program).GetMethods(BindingFlags.NonPublic |
+                                               BindingFlags.Static)
+                                   .Where(x => x.IsDefined(typeof (SampleAttribute), true))
+                                   .FirstOrDefault(x =>StringComparer.InvariantCultureIgnoreCase.Equals(x.GetCustomAttribute<SampleAttribute>().Name, sampleName));
+        }
+
+        [Sample("arguments")]
+        private static void Arguments(IWampRealmServiceProvider serviceProvider)
+        {
+            IArgumentsService proxy = serviceProvider.GetCalleeProxy<IArgumentsService>();
+
+            proxy.Ping();
+            Console.WriteLine("Pinged!");
+
+            int result = proxy.Add2(2, 3);
+            Console.WriteLine("Add2: {0}", result);
+
+            var starred = proxy.Stars();
+            Console.WriteLine("Starred 1: {0}", starred);
+
+            starred = proxy.Stars(nick: "Homer");
+            Console.WriteLine("Starred 2: {0}", starred);
+
+            starred = proxy.Stars(stars: 5);
+            Console.WriteLine("Starred 3: {0}", starred);
+
+            starred = proxy.Stars(nick: "Homer", stars: 5);
+            Console.WriteLine("Starred 4: {0}", starred);
+
+            string[] orders = proxy.Orders("coffee");
+            Console.WriteLine("Orders 1: {0}", string.Join(", ", orders));
+
+            orders = proxy.Orders("coffee", limit: 10);
+            Console.WriteLine("Orders 2: {0}", string.Join(", ", orders));
+        }
+
+        [Sample("complex")]
+        private static void Complex(IWampRealmServiceProvider serviceProvider)
+        {
+            IComplexResultService proxy = 
+                serviceProvider.GetCalleeProxy<IComplexResultService>();
+
+            string[] splitted = proxy.SplitName("George Bush");
+            Console.WriteLine("Pinged!");
+        }
+
     }
 }
