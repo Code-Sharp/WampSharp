6e8682ef4079557e0169c68aa20321a586a3ab8c
Elad Zelingher <elad@codesharp.net>
2018-03-10 00:12:03 -0500

    Hopefully this time it is really fixed

--- GitLinkPdb.bat
+++ GitLinkPdb.bat
@@ -3,6 +3,6 @@ setlocal enableDelayedExpansion
 
 set MYDIR=%1
 for /F %%x in ('dir /B/D/S %MYDIR%\*.pdb') do (
-  set FILENAME=%MYDIR%\%%x
+  set FILENAME=%%x
   GitLink\build\GitLink.exe !FILENAME!
 )
