13dc2cb2088db58b77328f010f1f772253e2cad0
Elad Zelingher <elad@codesharp.net>
2018-03-13 15:21:49 -0400

    Adding pcl to the script

--- GitLinkPcl.bat
+++ GitLinkPcl.bat
@@ -2,7 +2,7 @@
 setlocal enableDelayedExpansion 
 
 set MYDIR=%1
-for /F %%x in ('dir /B/D/S %MYDIR%\*.pdb') do (
+for /F %%x in ('dir /B/D/S %MYDIR%\pcl\*.pdb') do (
   set FILENAME=%%x
   GitLink\build\GitLink.exe !FILENAME!
 )
