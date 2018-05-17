08623f2eaf3ec50e69bf3cb0aba5449f97fe3ded
Tracy Harton <tharton@syn-apps.com>
2018-05-17 09:04:55 -0700

    Apply SecurityOptions to WebSocket4Net connections using MsgPack serialization, not just Json

--- src/net45/Default/WampSharp.WebSocket4Net/WAMP2/V2/Fluent/WebSocket4NetActivator.cs
+++ src/net45/Default/WampSharp.WebSocket4Net/WAMP2/V2/Fluent/WebSocket4NetActivator.cs
@@ -61,12 +61,24 @@ namespace WampSharp.V2.Fluent
 
         protected IControlledWampConnection<TMessage> CreateBinaryConnection<TMessage>(IWampBinaryBinding<TMessage> binaryBinding)
         {
-            return new WebSocket4NetBinaryConnection<TMessage>(mWebSocketFactory(binaryBinding.Name), binaryBinding);
+            return new WebSocket4NetBinaryConnection<TMessage>(ActivateWebSocket(binaryBinding), binaryBinding);
         }
 
         protected IControlledWampConnection<TMessage> CreateTextConnection<TMessage>(IWampTextBinding<TMessage> textBinding)
         {
             return new WebSocket4NetTextConnection<TMessage>(ActivateWebSocket(textBinding), textBinding);
+        }
+
+        private WebSocket ActivateWebSocket<TMessage>(IWampBinaryBinding<TMessage> binaryBinding)
+        {
+            WebSocket webSocket = mWebSocketFactory(binaryBinding.Name);
+
+            if (SecurityOptionsConfigureAction != null)
+            {
+                SecurityOptionsConfigureAction(webSocket.Security);
+            }
+
+            return webSocket;
         }
 
         private WebSocket ActivateWebSocket<TMessage>(IWampTextBinding<TMessage> textBinding)
