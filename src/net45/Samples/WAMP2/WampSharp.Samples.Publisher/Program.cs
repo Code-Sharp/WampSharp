63e1d2a2ee04a244d13bd403967ab9a455070070
Elad Zelingher <elad@codesharp.net>
2019-08-24 03:19:50 -0400

    Publisher complex example

--- src/net45/Samples/WAMP2/WampSharp.Samples.Publisher/Program.cs
+++ src/net45/Samples/WAMP2/WampSharp.Samples.Publisher/Program.cs
@@ -1,12 +1,30 @@
 ﻿using System;
+using System.Threading.Tasks;
+using WampSharp.V2;
 
 namespace WampSharp.Samples.Publisher
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            IWampChannel channel = SamplesArgumentParser.CreateWampChannel(args);
+
+            await channel.Open().ConfigureAwait(false);
+
+            ComplexPublisher publisher = new ComplexPublisher();
+
+            using (IDisposable disposable =
+                channel.RealmProxy.Services.RegisterPublisher(publisher))
+            {
+                Console.WriteLine("Hit enter to stop publishing");
+
+                Console.ReadLine();
+            }
+
+            Console.WriteLine("Stopped publishing");
+
+            Console.ReadLine();
         }
     }
 }
---
+++ src/net45/Samples/WAMP2/WampSharp.Samples.Publisher/Reflection/Complex.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Reactive.Linq;
+using Newtonsoft.Json;
+using WampSharp.V2.PubSub;
+
+namespace WampSharp.Samples
+{
+    public class MyClass
+    {
+        [JsonProperty("counter")]
+        public int Counter { get; set; }
+
+        [JsonProperty("foo")]
+        public int[] Foo { get; set; }
+    }
+
+    public delegate void MyPublicationDelegate(int number1, int number2, string c, MyClass d);
+
+    public interface IComplexPublisher
+    {
+        [WampTopic("com.myapp.heartbeat")]
+        event Action Heartbeat;
+
+        [WampTopic("com.myapp.topic2")]
+        event MyPublicationDelegate MyEvent;
+    }
+
+    public class ComplexPublisher : IComplexPublisher
+    {
+        private readonly Random mRandom = new Random();
+        private IDisposable mSubscription;
+
+        public ComplexPublisher()
+        {
+            mSubscription =
+                Observable.Timer(TimeSpan.FromSeconds(0),
+                                 TimeSpan.FromSeconds(1)).Select((x, i) => i)
+                          .Subscribe(x => OnTimer(x));
+        }
+
+        private void OnTimer(int value)
+        {
+            RaiseHeartbeat();
+
+            RaiseMyEvent(mRandom.Next(0, 100),
+                         23,
+                         "Hello",
+                         new MyClass()
+                         {
+                             Counter = value,
+                             Foo = new int[] {1, 2, 3}
+                         });
+        }
+
+        private void RaiseHeartbeat()
+        {
+            Heartbeat?.Invoke();
+        }
+
+        private void RaiseMyEvent(int number1, int number2, string c, MyClass d)
+        {
+            MyEvent?.Invoke(number1, number2, c, d);
+        }
+
+        public event Action Heartbeat;
+
+        public event MyPublicationDelegate MyEvent;
+    }
+}
