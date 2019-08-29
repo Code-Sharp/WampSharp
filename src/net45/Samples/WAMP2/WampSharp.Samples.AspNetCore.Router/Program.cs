3814deca6145c5516fc08972d71186dac9b9e1f3
Elad Zelingher <elad@codesharp.net>
2019-08-29 19:10:00 -0400

    Allowing to specify port, converting to CliFx

--- src/net45/Samples/WAMP2/WampSharp.Samples.AspNetCore.Router/Program.cs
+++ src/net45/Samples/WAMP2/WampSharp.Samples.AspNetCore.Router/Program.cs
@@ -1,70 +1,16 @@
-﻿using System.IO;
-using System.Net;
-using Microsoft.AspNetCore;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.FileProviders;
-using WampSharp.AspNetCore.RawSocket;
-using WampSharp.AspNetCore.WebSockets.Server;
-using WampSharp.Binding;
-using WampSharp.V2;
+﻿using System.Threading.Tasks;
+using CliFx;
 
 namespace WampSharp.Samples.AspNetCore.Router
 {
-    public class Program
+    class Program
     {
-        public static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            WampHost wampHost = new WampHost();
-
-            JTokenJsonBinding jsonBinding = new JTokenJsonBinding();
-            JTokenCborBinding cborBinding = new JTokenCborBinding();
-            JTokenMessagePackBinding messagePackBinding = new JTokenMessagePackBinding();
-
-            IWebHost host =
-                WebHost.CreateDefaultBuilder(args)
-                       .UseKestrel(options =>
-                                   {
-                                       options.Listen(IPAddress.Loopback, 8080,
-                                                      builder =>
-                                                      {
-                                                          // Log all of the http bytes as they are sent and received
-                                                          builder.UseConnectionLogging();
-
-                                                          // Configure RawSocket transport
-                                                          wampHost
-                                                              .RegisterTransport(new AspNetCoreRawSocketTransport(builder),
-                                                                                 jsonBinding,
-                                                                                 cborBinding,
-                                                                                 messagePackBinding);
-                                                      });
-                                   })
-                       .Configure(app =>
-                                  {
-                                      app.Map("/ws",
-                                              builder =>
-                                              {
-                                                  builder.UseWebSockets();
-
-                                                  // Configure WebSockets transport
-                                                  wampHost
-                                                      .RegisterTransport(new AspNetCoreWebSocketTransport(builder, null),
-                                                                         jsonBinding,
-                                                                         cborBinding,
-                                                                         messagePackBinding);
-                                              });
-
-                                      app.UseStaticFiles(new StaticFileOptions
-                                      {
-                                          FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), "static")),
-                                          RequestPath = ""
-                                      });
-
-                                      wampHost.Open();
-                                  })
-                       .Build();
-
-            host.Run();
+            await new CliApplicationBuilder()
+                  .AddCommandsFromThisAssembly()
+                  .Build()
+                  .RunAsync(args);
         }
     }
 }
---
+++ src/net45/Samples/WAMP2/WampSharp.Samples.AspNetCore.Router/RunRouterCommand.cs
@@ -0,0 +1,92 @@
+﻿using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+using CliFx;
+using CliFx.Attributes;
+using CliFx.Services;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.FileProviders;
+using WampSharp.AspNetCore.RawSocket;
+using WampSharp.AspNetCore.WebSockets.Server;
+using WampSharp.Binding;
+using WampSharp.V2;
+
+namespace WampSharp.Samples.AspNetCore.Router
+{
+    [Command("run", Description = "Runs a WAMP router, powered by ASP.NET Core.")]
+    public class RunRouterCommand : ICommand
+    {
+        [CommandOption("port", 'p', Description = "The port to listen on for requests. Default value is 8080.", IsRequired = false)]
+        public int Port { get; set; } = 8080;
+
+        public async Task ExecuteAsync(IConsole console)
+        {
+            WampHost wampHost = new WampHost();
+
+            JTokenJsonBinding jsonBinding = new JTokenJsonBinding();
+            JTokenCborBinding cborBinding = new JTokenCborBinding();
+            JTokenMessagePackBinding messagePackBinding = new JTokenMessagePackBinding();
+
+            IWebHost host =
+                WebHost.CreateDefaultBuilder()
+                       .UseKestrel(options =>
+                                   {
+                                       options.Listen(IPAddress.Loopback, Port,
+                                                      builder =>
+                                                      {
+                                                          // Log all of the http bytes as they are sent and received
+                                                          builder.UseConnectionLogging();
+
+                                                          // Configure RawSocket transport
+                                                          wampHost
+                                                              .RegisterTransport(new AspNetCoreRawSocketTransport(builder),
+                                                                                 jsonBinding,
+                                                                                 cborBinding,
+                                                                                 messagePackBinding);
+                                                      });
+                                   })
+                       .Configure(app =>
+                                  {
+                                      app.Map("/ws",
+                                              builder =>
+                                              {
+                                                  builder.UseWebSockets();
+
+                                                  // Configure WebSockets transport
+                                                  wampHost
+                                                      .RegisterTransport(new AspNetCoreWebSocketTransport(builder, null),
+                                                                         jsonBinding,
+                                                                         cborBinding,
+                                                                         messagePackBinding);
+                                              });
+
+                                      app.UseStaticFiles(new StaticFileOptions
+                                                         {
+                                                             FileProvider =
+                                                                 new
+                                                                     PhysicalFileProvider(Path
+                                                                                              .Combine(Directory.GetCurrentDirectory(),
+                                                                                                       "static")),
+                                                             RequestPath = ""
+                                                         });
+
+                                      app.UseDirectoryBrowser(new DirectoryBrowserOptions
+                                                              {
+                                                                  FileProvider =
+                                                                      new
+                                                                          PhysicalFileProvider(Path
+                                                                                                   .Combine(Directory.GetCurrentDirectory(),
+                                                                                                            "static")),
+                                                                  RequestPath = ""
+                                                              });
+
+                                      wampHost.Open();
+                                  })
+                       .Build();
+
+            await host.RunAsync().ConfigureAwait(false);
+        }
+    }
+}
