1a5bef8a63d1a3fd67aeb6f7088f4f1a139aa6e1
Elad Zelingher <elad@codesharp.net>
2022-06-09 17:49:12 -0400

    Forgot CancelTests.cs

--- src/netstandard/Tests/WampSharp.Tests.Wampv2/Integration/CancelTests.cs
+++ src/netstandard/Tests/WampSharp.Tests.Wampv2/Integration/CancelTests.cs
@@ -241,6 +241,14 @@ namespace WampSharp.Tests.Wampv2.Integration
                 InterruptCalled = true;
             }
 
+            public bool IsInvocationCompleted
+            {
+                get
+                {
+                    return false;
+                }
+            }
+
             public bool InterruptCalled { get; set; }
         }
     }
